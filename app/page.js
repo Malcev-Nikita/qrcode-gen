fda39bba53e344deddfba4df3c5311cfba15d616
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-12-13 19:01:45 +0400

    add container main block

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import QrCodeGen from './QrCodeGen'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between container m-auto pt-24">
       <QrCodeGen/>
     </main>
   )
